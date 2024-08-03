80740567331743292b83e1d0c0f6fc9ce62f110f
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-08-03 01:40:26 -0300

    server

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import "dotenv/config.js";
+
+import express from 'express';
+import cors from 'cors';
+import { queryBestPools } from './the-graph.service.mjs';
+
+const { THE_GRAPH_API_KEY, PORT, HOST_URL } = process.env;
+if (!THE_GRAPH_API_KEY) throw new Error('THE_GRAPH_API_KEY is not defined');
+
+const app = express();
+const port = PORT || 3000;
+const host = HOST_URL || 'http://localhost';
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/search', async (req, res) => {
+  const { tokenA, tokenB } = req.query;
+  const response = await queryBestPools({ tokenA, tokenB });
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${host}:${port}`);
+});
